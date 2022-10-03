df63e9f8f2ffac7f4b21bbceb82766df91de6d95
Chaks Malaks <u18202374@tuks.co.za>
2022-10-03 09:23:36 +0200

    AccountModel created

---
+++ PracticalDay/Model/AccountModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PracticalDay.Model;
+
+public class AccountModel
+{
+    public Guid UserId { get; set; }
+    
+    public String Username { get; set;}
+    
+    public String Password { get; set;}
+
+}
